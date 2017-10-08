f16e5c444dd20ec9da12f913189b8dc9abe2ad58
Derek Kramer <derek.kramer88@gmail.com>
2017-10-07 23:32:41 -0600

    🖼 Canvas displaying API data

--- src/App.js
+++ src/App.js
@@ -5,76 +5,57 @@ const axios = require('axios');
 const $ = require('jquery');
 
 class App extends Component {
-    componentWillMount() {
-        let quilt = [];
-        let canvas;
-        let region;
-        let block;
-        let pixel;
+    componentDidMount() {
+        const canvas = document.getElementById('quilt');
+        canvas.width = 2000;
+        canvas.height = 2000;
+        const ctx = canvas.getContext('2d');
+        ctx.scale(2,2);
 
         axios.get('http://localhost:3001/')
         .then(result => {
             let iterator = 0;
 
-            for(let r = 0; r < 6; r++){
-                quilt.push([]);
-                for(let b = 0; b < 100; b++){
-                    quilt[r].push([])
-                    for(let row = 0; row < 50; row++){
-                        quilt[r][b].push([]);
-                        let rowColors = result.data[iterator].color.split(',');
+            for(let b = 0; b < 100; b++){
+                for(let row = 0; row < 50; row++){
+                    let rowColors = result.data[iterator].color.split(',');
 
-                        rowColors.forEach(color => quilt[r][b][row].push(color));
+                    for(let col = 0; col < 50; col++){
+                        let loc = getLocation(b, row, col);
 
-                        iterator++;
+                        ctx.fillStyle = `#${rowColors[col]}`;
+                        ctx.fillRect(loc.x, loc.y, 2, 2);
                     }
+
+                    iterator++;
                 }
             }
         })
-        // .then(result => {
-        //     let iterator = 0;
-        //     canvas = document.getElementById('quilt');
-        //
-        //     for(let r = 0; r < 4; r++){
-        //         region = document.createElement('div');
-        //         region.setAttribute('class', 'region');
-        //
-        //         for(let b = 0; b < 100; b++){
-        //             block = document.createElement('div');
-        //             block.setAttribute('class', 'block');
-        //
-        //             for(let row = 0; row < 50; row++){
-        //                 let rowColors = result.data[iterator].color.split(',');
-        //
-        //                 rowColors.forEach(color => {
-        //                     color = `#${color}`;
-        //                     pixel = document.createElement('div');
-        //                     pixel.setAttribute('class', 'pixel');
-        //                     pixel.style.backgroundColor = color;
-        //
-        //                     block.appendChild(pixel);
-        //                 });
-        //
-        //                 iterator++;
-        //             }
-        //
-        //             region.appendChild(block);
-        //         }
-        //
-        //         canvas.appendChild(region);
-        //     }
-        // })
-        .catch(err => console.error(new Error(err)));
+        .then(() => {
+            $('#quilt').click((event) => {
+                ctx.fillStyle = '#ff0000';
+                ctx.fillRect(500, 500, 2, 2);
+            });
+        })
+        .catch(err => console.error(err));
     }
 
     render() {
         return (
-            <div className="App">
-                <h1>PixelShare</h1>
-                <div id='quilt'></div>
+            <div className='App'>
+                <div className='container'>
+                    <canvas id='quilt'></canvas>
+                </div>
             </div>
         );
     }
+}
+
+function getLocation(b, row, col){
+    let x = ((b % 10) * 100) + col * 2;
+    let y = (Math.floor(b / 10) * 100) + row * 2;
+
+    return {x, y};
 }
 
 export default App;
